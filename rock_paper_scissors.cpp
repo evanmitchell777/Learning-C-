53e64d5377bed9bafda01140d70bba1699ad6ff3
Evan Mitchell <43766252+evanmitchell777@users.noreply.github.com>
2023-02-23 11:26:59 -0600

    Update rock_paper_scissors.cpp

--- rock_paper_scissors.cpp
+++ rock_paper_scissors.cpp
@@ -5,40 +5,46 @@
 
 using namespace std;
 
-string choices[3]={"rock","paper","scissors"};
+string choices[3] = {"rock", "paper", "scissors"};
 
 int main(){
-srand (time(NULL));
-int RandIndex = rand() %5;
-string cc = choices[RandIndex];
+    srand(time(NULL)); // call srand() only once at the beginning of the program
 
+    string play_again = "y";
 
-cout<<"Enter rock, paper or scissors: ";
-string user_choice;
-cin>>user_choice;
+    while(play_again == "y"){
+        int RandIndex = rand() % 3;
+        string cc = choices[RandIndex];
+       
 
-cout<<"I choose "<< cc;
+         cout<<"Enter rock, paper or scissors: ";
+         string user_choice;
+          cin>>user_choice;
+       cout << "I choose " << cc;
+        if (user_choice == "rock" && cc == "rock")
+            cout << " tie";
+        else if  (user_choice == "rock" && cc == "paper")
+            cout << " you lose";
+        else if  (user_choice == "rock" && cc == "scissors")
+            cout << " you win";
+        else if (user_choice == "paper" && cc == "paper")
+            cout << " tie";
+        else if  (user_choice == "paper" && cc == "rock")
+            cout << " you win";
+        else if (user_choice == "paper" && cc == "scissors")
+            cout << " you lose";
+        else if (user_choice == "scissors" && cc == "scissors")
+            cout << " tie";
+        else if (user_choice == "scissors" && cc == "rock")
+            cout << " you lose";
+        else if (user_choice == "scissors" && cc == "paper")
+            cout << " you win";
+        else
+            cout << " enter a valid choice";
 
-if (user_choice == "rock" && cc == "rock")
-    cout<<" tie";
-    else if  (user_choice == "rock" && cc =="paper")
-    cout<<" you lose";
-else if  (user_choice == "rock" && cc =="scissors")
-    cout<<" you win";
-else if (user_choice=="paper" && cc=="paper")
-    cout<<" tie";
-else if  (user_choice=="paper" && cc=="rock")
-    cout<<" tie";
-else if (user_choice =="paper" && cc == "scissors")
-    cout<<" you lose";
-else if( user_choice == "scissors" && cc== "scissors")
-    cout<<" tie";
-else if (user_choice=="scissors" && cc=="rock")
-    cout<<" you lose";
-else if  (user_choice== "scissors" && cc=="paper")
-    cout<<" you win";
-
-else
-    cout<< "enter a valid choice";
-return 0;
+        cout << "\nWould you like to play again? Enter y or n: ";
+        cin >> play_again;
+    }
+    
+    return 0;
 }
